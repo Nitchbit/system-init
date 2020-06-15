83502e830fef789df0482c126378a316eadefd7e
Snjch <tuminonico@gmail.com>
2020-06-15 15:16:16 +0200

    feat: added removeall option

--- configscript.sh
+++ configscript.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-# Define function
-Configure() {
-	# Root
-	#sudo -i
-
+if [[ $1 = "-removeall" ]]; then
+	sudo apt-get purge -y openjdk-8-jdk
+	sudo apt-get purge -y wireshark
+	sudo apt-get purge -y ardour
+	sudo snap remove android-studio
+	sudo snap remove intellij-idea-community
+	sudo snap remove pycharm-community
+	#sudo snap remove sublime-text
+	sudo snap remove blender
+	sudo snap remove gimp
+	sudo snap remove kdenlive
+	sudo apt-get autoremove -y
+else 
 	# Update the system
-	apt-get update
-	apt-get upgrade
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
 
-	apt-get install snapd
+	# Install sudo snapd
+	sudo apt-get install -y snapd
 
 	# Android Studio
-	apt-get install openjdk-8-jdk
-	snap install --classic android-studio
+	sudo apt-get install -y openjdk-8-jdk
+	sudo snap install --classic android-studio
 
 	# IntelliJ
-	snap install --classic intellij-idea-community 
+	sudo snap install --classic intellij-idea-community 
 
 	# PycCarhm
-	snap install --classic pycharm-community
+	sudo snap install --classic pycharm-community
 
 	# Sublime Text
-	snap install --classic sublime-text
+	sudo snap install --classic sublime-text
 
 	# Wireshark
-	apt-get install wireshark
+	sudo apt-get install -y wireshark
 
 	# Ardour
-	apt-get install ardour
+	sudo apt-get install -y ardour
 
 	# Blender
-	snap install --classic blender
+	sudo snap install --classic blender
 
 	# Gimp
-	snap install gimp
+	sudo snap install gimp
 
 	# Kdenlive
-	snap install kdenlive
+	sudo snap install kdenlive
 
 	# Update the system
-	apt-get update
-	apt-get upgrade
-	atp-get autoremove
-	snap refresh
-
-	#exit
-}
-
-# Invoke function
-Configure
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
+	sudo apt-get autoremove -y
+	sudo snap refresh
+fi
