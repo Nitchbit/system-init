ab45e886f199db37d34fdfe8efe37752dfe98587
Nicolò Tumino <64739080+Snjch@users.noreply.github.com>
2020-06-15 15:53:28 +0200

    Update configscript.sh

--- configscript.sh
+++ configscript.sh
@@ -15,7 +15,44 @@ if [[ $1 = "-removeall" ]]; then
 	sudo snap remove blender
 	sudo snap remove gimp
 	sudo snap remove kdenlive
+
+elif [[ $1 = "-productivity" ]]; then
+	# Update the system
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
 	
+	# Install sudo snapd
+	sudo apt-get install -y snapd
+	
+	# Android Studio
+	sudo apt-get install -y openjdk-8-jdk
+	sudo snap install --classic android-studio
+
+	# IntelliJ
+	sudo snap install --classic intellij-idea-community 
+
+	# PycCarhm
+	sudo snap install --classic pycharm-community
+
+	# Sublime Text
+	sudo snap install --classic sublime-text
+
+elif [[ $1 = "-creativity" ]]; then
+	# Wireshark
+	sudo apt-get install -y wireshark
+
+	# Ardour
+	sudo apt-get install -y ardour
+
+	# Blender
+	sudo snap install --classic blender
+
+	# Gimp
+	sudo snap install gimp
+
+	# Kdenlive
+	sudo snap install kdenlive
+
 # Installing all
 else 
 	# Update the system
@@ -52,7 +89,15 @@ else
 
 	# Kdenlive
 	sudo snap install kdenlive
-
+	
+	# Spotify
+	sudo snap install spotify
+	
+	# Telegram Desktop
+	sudo snap install telegram-desktop
+	
+	# WhatsApp Desktop
+	sudo snap install whatsdesk
 fi
 
 # Update the system
