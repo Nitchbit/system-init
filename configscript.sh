c049a7367b23f534494d17851ce4f4e3aaccb447
Snjch <tuminonico@gmail.com>
2020-06-15 13:37:31 +0200

    feat: added command to install softwares

--- configscript.sh
+++ configscript.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+# Root
+sudo -i
+
+# Update the system
+apt-get update
+apt-get upgrade
+
+apt-get install snapd
 
 # Android Studio
-sudo apt-get install openjdk-8-jdk
-sudo snap install --classic android-studio
+apt-get install openjdk-8-jdk
+snap install --classic android-studio
 
 # IntelliJ
+snap install --classic intellij-idea-community 
 
 # PycCarhm
+snap install --classic pycharm-community
+
 # Sublime Text
+snap install --classic sublime-text
 
 # Wireshark
+apt-get install wireshark
 
 # Ardour
+apt-get install ardour
+
 # Blender
+snap install --classic blender
+
 # Gimp
+snap install gimp
+
 # Kdenlive
+snap install kdenlive
+
+# Update the system
+apt-get update
+apt-get upgrade
+atp-get autoremove
+snap refresh
+
+exit
