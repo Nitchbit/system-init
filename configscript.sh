507fff1443e16ceb362e0433f126e83ee9f97b14
Nicolò Tumino <64739080+Snjch@users.noreply.github.com>
2020-06-15 12:01:06 +0200

    Initial commit

---
+++ configscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# Android Studio
+sudo apt-get install openjdk-8-jdk
+sudo snap install --classic android-studio
+
+# IntelliJ
+
+# PycCarhm
+# Sublime Text
+
+# Wireshark
+
+# Ardour
+# Blender
+# Gimp
+# Kdenlive
