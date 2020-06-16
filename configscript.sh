13b95e865f91ddb93b61e20a13215e9abe5372a7
Snjch <snjch@base32>
2020-06-16 11:19:21 +0200

    improvement: changed the structure to make the script customizable

--- configscript.sh
+++ configscript.sh
@@ -3,110 +3,49 @@
 # Configuring dpkg
 sudo dpkg --configure -a
 
-# Removing all
-if [[ $1 = "-removeall" ]]; then
-	sudo apt purge -y wireshark
-	sudo apt purge -y ardour
-	sudo snap remove android-studio
-	sudo snap remove intellij-idea-community
-	sudo snap remove pycharm-community
-	sudo snap remove sublime-text
-	sudo snap remove blender
-	sudo snap remove gimp
-	sudo snap remove kdenlive
-	sudo apt purge -y snapd
-	sudo apt purge -y gparted
-fi
-
-# Update the system
+# Updating the system
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y snapd
-sudo apt install -y gparted
-
-# Installing productivity softwares
-if [[ $1 = "-productivity" ]]; then
-	# Android Studio
-	sudo snap install --classic android-studio
-
-	# IntelliJ
-	sudo snap install --classic intellij-idea-community 
-
-	# PycCarhm
-	sudo snap install --classic pycharm-community
-
-	# Sublime Text
-	sudo snap install --classic sublime-text
-
-# Installing creativity softwares
-elif [[ $1 = "-creativity" ]]; then
-	# Wireshark
-	sudo apt install -y wireshark
-
-	# Ardour
-	sudo apt install -y ardour
-
-	# Blender
-	sudo snap install --classic blender
-
-	# Gimp
-	sudo snap install gimp
-
-	# Kdenlive
-	sudo snap install kdenlive
-
-# Installing all
-else 
-	# Install sudo snapd
-	sudo apt install -y snapd
-
-	# Android Studio
-	sudo apt install -y openjdk-8-jdk
-	sudo snap install --classic android-studio
-
-	# IntelliJ
-	sudo snap install --classic intellij-idea-community 
-
-	# PycCarhm
-	sudo snap install --classic pycharm-community
-
-	# Sublime Text
-	sudo snap install --classic sublime-text
-
-	# Wireshark
-	sudo apt install -y wireshark
-
-	# Ardour
-	sudo apt install -y ardour
-
-	# Blender
-	sudo snap install --classic blender
-
-	# Gimp
-	sudo snap install gimp
-
-	# Kdenlive
-	sudo snap install kdenlive
-	
-	# Spotify
-	sudo snap install spotify
-	
-	# Telegram Desktop
-	sudo snap install telegram-desktop
-	
-	# WhatsApp Desktop
-	sudo snap install whatsdesk
-	
-	# Gnome Tweak Tool
-	sudo apt install -y gnome-tweak-tool
-	
-	# Neofetch
-	sudo apt install -y neofetch
-fi
-
-# Update the system
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autoremove -y
 sudo apt full-upgrade -y
+
+# Installing snapd
+sudo apt install -y snapd
+sudo apt install -y neofetch
+
+FLAG=0
+while read line; do
+	if [[ $line = "#ToInstall" ]]; then
+		FLAG=1
+		continue;
+	fi
+	if [[ $line = "#ToRemove" ]]; then
+		FLAG=-1
+		continue;
+	fi
+	if [[ $FLAG = 1 ]]; then
+		if sudo snap install $line; then
+			continue;
+		else
+			sudo apt install -y $line;
+			continue;
+		fi
+	fi
+	if [[ $FLAG = -1 ]]; then
+		if sudo snap remove $line; then
+			continue;
+		else 
+			sudo apt purge -y $line;
+			continue;
+		fi
+	fi
+done < $PWD/init.config
+
+# Updating the system
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
 sudo snap refresh
+
+# Executing neofetch
+sudo neofetch
